d0891bb05e8c0c9fad0e29c6c1220bb7c5dfe41a
Yerania Hernandez <hernandez.yerania2@gmail.com>
2019-03-18 14:27:44 -0500

    Implemented CRC checker

---
+++ helper_function.h
@@ -0,0 +1,20 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+using namespace std;
+
+uint64_t CRC(uint64_t address){
+	unsigned long long crcPolynomial = 3988292384ULL;  //Decimal value for 0xEDB88320 hex value
+    unsigned long long result = address;
+    for(unsigned int i = 0; i < 32; i++ )
+    	if(( _returnVal & 1 ) == 1 ){
+    		result = (result >> 1) ^ crcPolynomial;
+    	}
+    	else{
+    		result >>= 1;
+    	}
+    return result;
+}
+
+
+#endif
